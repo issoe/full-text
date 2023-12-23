42a4ff615d8fd51330a30469c7c44e5379de4737
issoe <twmnno@gmail.com>
2023-12-24 03:48:04 +0700

    remove pdf lib react

--- fe-react-js/src/components/nav/BaseNav.js
+++ fe-react-js/src/components/nav/BaseNav.js
@@ -35,27 +35,27 @@ function BaseNav({ active }) {
           return <NavItem item={item} key={index} active={active} />;
         })}
       </NavList> */}
-      {/* Welcome user */}
-      {/* <div className='font-bold text-lg mt-2'>Details</div> */}
-      {/* <div className="w-full h-[1px] bg-[#FFF]"></div> */}
-      {/* <ul className='list-disc relative'> */}
-        {/* <li className='ml-4'>Keyword: xyz</li> */}
-        {/* <li className='ml-4'>File: SRS file name.pdf</li> */}
-        {/* <li className='ml-4'>Created: khanhpq@gmail.com</li> */}
-        {/* <li className='ml-4'>Date: 20-10-2023</li> */}
-        {/* <li className='ml-4'>Views: 2023</li> */}
-      {/* </ul> */}
+      Welcome <div className='inline-block underline'>khanhpq.fs@gmail.com</div>
+      <div className='font-bold text-lg mt-2'>Details</div>
+      <div className="w-full h-[1px] bg-[#FFF]"></div>
+      <ul className='list-disc relative'>
+        <li className='ml-4'>Keyword: xyz</li>
+        <li className='ml-4'>File: SRS file name.pdf</li>
+        <li className='ml-4'>Created: khanhpq@gmail.com</li>
+        <li className='ml-4'>Date: 20-10-2023</li>
+        <li className='ml-4'>Views: 2023</li>
+      </ul>
       {/* <div className='w-full grid justify-items-center my-1'><button className='bg-[red] p-1 rounded-lg '>Download</button></div> */}
       
-      <div className='font-bold text-lg mt-2'>Top hits</div>
-      <div className="w-full h-[1px] bg-[#FFF]"></div>
+      <div className='font-bold text-lg mt-2'>Top trending</div>
+      <div className="w-full h-[1px] bg-[#FFF] my-1"></div>
             {topHits?.map((item, index) => {
               return (
                 <Paper key={index} keyword={item.keyword}>
-                  <div className="relative">
-                    <div className='inline-block bg-[#6465F1] px-2 m-1 rounded-full'>{index + 1}</div>
-                    {item.keyword}
-                    <strong className="absolute right-[0px]">{item.count}</strong>
+                  <div className="relative hover:bg-[#6465F1]">
+                    <div className='inline-block bg-[#6465F1] px-2 my-1 mx-2 rounded-full'>{index + 1}</div>
+                    <div className='inline-block'>{item.keyword}</div>
+                    <strong className="absolute right-[0px] mr-2">{item.count}</strong>
                   </div>
                 </Paper>
               );
